f678e87734ee2f8a00c3a5c8802d7a1e07baf494
tbui-quo <tbui-quo@student.42wolfsburg.de>
2024-06-25 18:50:45 +0200

    added cmakelist and added git ignore stuff

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tbui-quo <tbui-quo@student.42wolfsburg.d>  +#+  +:+       +#+        */
+/*   By: tbui-quo <tbui-quo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 18:44:17 by tbui-quo          #+#    #+#             */
-/*   Updated: 2023/07/04 18:07:18 by tbui-quo         ###   ########.fr       */
+/*   Updated: 2024/06/25 18:36:28 by tbui-quo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int	main(void)
 {
-	ft_printf("test\n");
-	ft_putstr("a\n");
+	printf("test\n");
 	return (0);
 }

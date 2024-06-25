ae37d7fad649673d585209fa2715358fc2758131
tbui-quo <tbui-quo@student.42wolfsburg.de>
2024-06-25 18:36:18 +0200

    rip no libft allowed

--- lib/philosophers.h
+++ lib/philosophers.h
@@ -6,15 +6,13 @@
 /*   By: tbui-quo <tbui-quo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 14:44:37 by tbui-quo          #+#    #+#             */
-/*   Updated: 2024/06/25 18:32:33 by tbui-quo         ###   ########.fr       */
+/*   Updated: 2024/06/25 18:35:41 by tbui-quo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-# include "../lib/libft/libft.h"
-# include "../lib/ft_printf/ft_printf.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>

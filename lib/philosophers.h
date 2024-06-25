c1f73f61a616489e6a8be8842591b6feb45a8450
tbui-quo <tbui-quo@student.42wolfsburg.de>
2024-06-25 18:33:05 +0200

    first setup

---
+++ lib/philosophers.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbui-quo <tbui-quo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 14:44:37 by tbui-quo          #+#    #+#             */
+/*   Updated: 2024/06/25 18:32:33 by tbui-quo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include "../lib/libft/libft.h"
+# include "../lib/ft_printf/ft_printf.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdbool.h>
+# include <limits.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
